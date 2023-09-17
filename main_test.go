7b18309acba91bda72b772666a9a87be728f610b
yawnak <shliakhov.danylo@gmail.com>
2023-09-17 21:49:30 +0300

    feat: Benchmark for person row unmarshal

--- main_test.go
+++ main_test.go
@@ -1,5 +1,43 @@
 package main
 
 import (
+	"testing"
+
 	"cloud.google.com/go/spanner"
 )
+
+func BenchmarkRowToStruct(b *testing.B) {
+	row, err := spanner.NewRow(rowColumnNames, rowColumnValues)
+	if err != nil {
+		b.Fatal("error creating row:", err)
+	}
+	var p Person
+
+	// Reset the timer to ignore initialization overhead.
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		row.ToStruct(&p)
+	}
+}
+
+func BenchmarkRowColumns(b *testing.B) {
+	row, err := spanner.NewRow(rowColumnNames, rowColumnValues)
+	if err != nil {
+		b.Fatal("error creating row:", err)
+	}
+	var p Person
+
+	// Reset the timer to ignore initialization overhead.
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		row.Columns(
+			&p.ID,
+			&p.Name,
+			&p.Age,
+			&p.Weight,
+			&p.IsMarried,
+		)
+	}
+}
