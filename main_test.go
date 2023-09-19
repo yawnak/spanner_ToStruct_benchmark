3ec5f8997a3c88aac04b294af1cf20500591daba
yawnak <shliakhov.danylo@gmail.com>
2023-09-19 11:32:44 +0300

    feat: Benchmarks for variadic row.Columns

--- main_test.go
+++ main_test.go
@@ -42,6 +42,18 @@ func BenchmarkRowColumns(b *testing.B) {
 	}
 }
 
+func BenchmarkColumnVariadic(b *testing.B) {
+	row, err := spanner.NewRow(rowColumnNames, rowColumnValues)
+	if err != nil {
+		b.Fatal("error creating row:", err)
+	}
+
+	var p Person
+	for i := 0; i < b.N; i++ {
+		row.Columns(p.Columns()...)
+	}
+}
+
 func BenchmarkRowToStructBig(b *testing.B) {
 	row, err := spanner.NewRow(bigRowColumnNames, bigRowColumnValues)
 	if err != nil {
@@ -103,4 +115,20 @@ func BenchmarkRowColumnsBig(b *testing.B) {
 			&p.FavoriteElement,
 		)
 	}
+}
+
+func BenchmarkColumnVariadicBig(b *testing.B) {
+	row, err := spanner.NewRow(bigRowColumnNames, bigRowColumnValues)
+	if err != nil {
+		b.Fatal("error creating row:", err)
+	}
+
+	var p BigPerson
+
+	// Reset the timer to ignore initialization overhead.
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		row.Columns(p.Columns()...)
+	}
+
 }
